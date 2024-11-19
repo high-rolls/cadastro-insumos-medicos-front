588e9fe43a9506e043c72ba77858f42963e3bbe1
Paulo Vitor F. G. Pivotto <paulo_vitor652@hotmail.com>
2024-11-19 14:24:56 -0300

    add card action buttons

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -4,7 +4,17 @@ import { asCurrency } from './utils/numbers'
 export default function ProductCard({ product }) {
     return (
         <article className={styles.card}>
-            <h2 className={styles.title}>{product.name}</h2>
+            <header className={styles.cardHeader}>
+                <h2 className={styles.title}>{product.name}</h2>
+                <div className={styles.actionButtons}>
+                    <a href="#" className={styles.actionButton}>
+                        <i className="bi bi-pencil-square"></i>
+                    </a>
+                    <a href="#" className={styles.actionButtonDelete}>
+                        <i className="bi bi-x-lg"></i>
+                    </a>
+                </div>
+            </header>
             <ul>
                 <li>quantidade: {product.quantity}</li>
                 <li>preço: {asCurrency(product.price)}
