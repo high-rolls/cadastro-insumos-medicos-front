af12008abf85adf9a2b35dda9e68c646945d1455
Paulo Vitor F. G. Pivotto <paulo_vitor652@hotmail.com>
2024-11-19 15:44:47 -0300

    add edit button feature

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -1,25 +1,52 @@
+import { useState } from 'react'
 import styles from './ProductCard.module.css'
 import { asCurrency } from './utils/numbers'
 
 export default function ProductCard({ product }) {
+    const [isEditing, setIsEditing] = useState(false)
+    const [name, setName] = useState(product.name)
+
+    const handleEditClicked = (event) => {
+        setIsEditing(!isEditing)
+    }
+
     return (
         <article className={styles.card}>
             <header className={styles.cardHeader}>
-                <h2 className={styles.title}>{product.name}</h2>
-                <div className={styles.actionButtons}>
-                    <a href="#" className={styles.actionButton}>
-                        <i className="bi bi-pencil-square"></i>
-                    </a>
-                    <a href="#" className={styles.actionButtonDelete}>
-                        <i className="bi bi-x-lg"></i>
-                    </a>
-                </div>
+                {
+                    isEditing ?
+                        <input
+                            type="text"
+                            value={name}
+                            onChange={(event) => setName(event.target.value)}
+                        /> :
+                        <h2 className={styles.title}>{name}</h2>
+                }
+                {!isEditing &&
+                    <div className={styles.actionBar}>
+                        <a
+                            href="#"
+                            className={styles.actionLink}
+                            onClick={handleEditClicked}
+                        >
+                            <i className="bi bi-pencil-square"></i>
+                        </a>
+                        <a href="#" className={styles.actionLinkDelete}>
+                            <i className="bi bi-x-lg"></i>
+                        </a>
+                    </div>
+                }
             </header>
             <ul>
                 <li>quantidade: {product.quantity}</li>
                 <li>preço: {asCurrency(product.price)}
                 </li>
             </ul>
+            <div className={styles.bottomActionBar}>
+                <button type="button" className={styles.actionButton}>
+                    <i className="bi bi-check-lg"></i>
+                </button>
+            </div>
         </article>
     )
 }
