99a7b4c7ebb17e99a15dce139e097f3a2e46bc76
Paulo Vitor F. G. Pivotto <paulo_vitor652@hotmail.com>
2024-11-23 15:22:43 -0300

    Revert "toast ao atualizar um produto"
    This reverts commit 872bb3db6a5c012851348f5c4a31eca47bef8509.

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,6 @@ function App() {
         optimisticData: newProducts,
         ...mutateOptions
       })
-      toast.success('Produto atualizado.')
     } catch (e) {
       toast.error('Falha ao salvar produto.')
       console.error(e)
